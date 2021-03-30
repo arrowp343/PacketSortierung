5f1bbf52dfea9583ffb433ccc879ee7d0d2c8847
arrowp343 <67070734+arrowp343@users.noreply.github.com>
2021-03-31 00:08:05 +0200

    implement Pallet

--- src/main/java/Pallet.java
+++ src/main/java/Pallet.java
@@ -1,2 +1,21 @@
 public class Pallet {
+    static int lastId;
+    private final int id;
+    private Box[][][] boxes;
+
+    public Pallet(Box[][][] boxes){
+        id = ++lastId;
+        this.boxes = boxes;
+    }
+
+    /*public Pallet(String csv){
+        TODO einlesen aus csv
+    }*/
+
+    public int getId(){
+        return id;
+    }
+    public Box[][][] getBoxes(){
+        return boxes;
+    }
 }
