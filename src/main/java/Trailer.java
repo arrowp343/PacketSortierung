1a408c56864931332c1693f5a548e0fa0a67ee73
arrowp343 <67070734+arrowp343@users.noreply.github.com>
2021-03-31 08:54:12 +0200

    implement Truck + Trailer

---
+++ src/main/java/Trailer.java
@@ -0,0 +1,30 @@
+public class Trailer {
+    private Pallet[] palletsLeft, palletsRight;
+    private Truck truck;
+
+    public Trailer(Pallet[] palletsLeft, Pallet[] palletsRight){
+        this.palletsLeft = palletsLeft;
+        this.palletsRight = palletsRight;
+        truck = new Truck(this);
+    }
+
+    public Pallet[] getPalletsLeft(){
+        return palletsLeft;
+    }
+    public void setPalletsLeft(Pallet[] palletsLeft){
+        this.palletsLeft = palletsLeft;
+    }
+    public Pallet[] getPalletsRight(){
+        return palletsRight;
+    }
+    public void setPalletsRight(Pallet[] palletsRight){
+        this.palletsRight = palletsRight;
+    }
+    public Truck getTruck(){
+        return truck;
+    }
+    public void setTruck(Truck truck){
+        this.truck = truck;
+        truck.setTrailer(this);
+    }
+}
--- src/main/java/Truck.java
+++ src/main/java/Truck.java
@@ -1,2 +1,34 @@
 public class Truck {
+    private final String id;
+    private Trailer trailer;
+
+    private final char[] idCharPool;
+
+    public Truck(Trailer trailer){
+        idCharPool = Tools.connectCharPool(Tools.generateCharPool('a', 'z'), Tools.generateCharPool('0', '9'));
+        id = generateId();
+        this.trailer = trailer;
+    }
+
+    /*public Package(String csv){
+        TODO einlesen aus csv
+    }*/
+
+    private String generateId() {
+        char[] charId = new char[4];
+        for (int i = 0; i < charId.length; i++)
+            charId[i] = idCharPool[(int) (Math.random() * idCharPool.length)];
+        //TODO check if already used
+        return new String(charId);
+    }
+
+    public String getId(){
+        return id;
+    }
+    public Trailer getTrailer(){
+        return trailer;
+    }
+    public void setTrailer(Trailer trailer){
+        this.trailer = trailer;
+    }
 }
