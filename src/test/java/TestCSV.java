9df60110dd940392d45d329751e21360f311c260
arrowp343 <67070734+arrowp343@users.noreply.github.com>
2021-04-06 13:22:46 +0200

    finish testBoxCSV

--- src/test/java/TestCSV.java
+++ src/test/java/TestCSV.java
@@ -12,11 +12,11 @@ public class TestCSV {
     @Order(1)
     public void testPackageCSV(){
         //write packages
-        Package[] packages = new Package[amountPackages];
-        for(int i = 0; i < packages.length; i++)
-            packages[i] = new Package();
+        Package[] expect = new Package[amountPackages];
+        for(int i = 0; i < expect.length; i++)
+            expect[i] = new Package();
         try{
-            CSV.writePackageInCSV(packages);
+            CSV.writePackageInCSV(expect);
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -27,15 +27,15 @@ public class TestCSV {
 
             //compare package arrays
             for (int i = 0; i < actual.length; i++) {
-                assertEquals(packages[i].getId(), actual[i].getId());
-                char[][][] e = packages[i].getContent(), a = actual[i].getContent();
+                assertEquals(expect[i].getId(), actual[i].getId());
+                char[][][] e = expect[i].getContent(), a = actual[i].getContent();
                 for (int i1 = 0; i1 < Configuration.l; i1++)
                     for (int i2 = 0; i2 < Configuration.w; i2++)
                         for (int i3 = 0; i3 < Configuration.h; i3++)
                             assertEquals(e[i1][i2][i3], a[i1][i2][i3]);
-                assertEquals(packages[i].getZip_code(), actual[i].getZip_code());
-                assertEquals(packages[i].getType(), actual[i].getType());
-                assertEquals(packages[i].getWeight(), actual[i].getWeight());
+                assertEquals(expect[i].getZip_code(), actual[i].getZip_code());
+                assertEquals(expect[i].getType(), actual[i].getType());
+                assertEquals(expect[i].getWeight(), actual[i].getWeight());
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -47,17 +47,33 @@ public class TestCSV {
         try{
             Package[] packages = CSV.readPackageFromCSV();
             int pCount = 0;
-            Box[] boxes = new Box[amountBoxes];
-            for(int i = 0; i < boxes.length; i++) {
+            Box[] expect = new Box[amountBoxes];
+            for(int i = 0; i < expect.length; i++) {
                 Package[] p = new Package[Configuration.maxPackagesInBox];
                 for(int k = 0; k < p.length; k++) {
                     p[k] = packages[pCount];
                     pCount++;
                 }
-                boxes[i] = new Box(p);
+                expect[i] = new Box(p);
             }
-            CSV.writeBoxInCSV(boxes);
-            //TODO readBoxFromCSV
+            CSV.writeBoxInCSV(expect);
+
+            Box[] actual = CSV.readBoxFromCSV();
+            for(int i = 0; i < expect.length; i++){
+                assertEquals(expect[i].getId(), actual[i].getId());
+                Package[] expectPackages = expect[i].getPackages(), actualPackages = actual[i].getPackages();
+                for (int j = 0; j < actualPackages.length; j++) {
+                    assertEquals(expectPackages[j].getId(), actualPackages[j].getId());
+                    char[][][] e = expectPackages[j].getContent(), a = actualPackages[j].getContent();
+                    for (int i1 = 0; i1 < Configuration.l; i1++)
+                        for (int i2 = 0; i2 < Configuration.w; i2++)
+                            for (int i3 = 0; i3 < Configuration.h; i3++)
+                                assertEquals(e[i1][i2][i3], a[i1][i2][i3]);
+                    assertEquals(expectPackages[j].getZip_code(), actualPackages[j].getZip_code());
+                    assertEquals(expectPackages[j].getType(), actualPackages[j].getType());
+                    assertEquals(expectPackages[j].getWeight(), actualPackages[j].getWeight());
+                }
+            }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
