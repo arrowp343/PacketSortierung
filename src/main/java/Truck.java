6f1c0b2285915468e336858ea564cfeaad0f7438
arrowp343 <67070734+arrowp343@users.noreply.github.com>
2021-04-07 15:50:14 +0200

    implement truck csv

--- src/main/java/Truck.java
+++ src/main/java/Truck.java
@@ -2,10 +2,7 @@ public class Truck {
     private final String id;
     private Trailer trailer;
 
-    private final char[] idCharPool;
-
     public Truck(Trailer trailer){
-        idCharPool = Tools.connectCharPool(Tools.generateCharPool('a', 'z'), Tools.generateCharPool('0', '9'));
         id = generateId();
         this.trailer = trailer;
     }
@@ -14,8 +11,9 @@ public class Truck {
         TODO einlesen aus csv
     }*/
 
-    private String generateId() {
+    public static String generateId() {
         char[] charId = new char[4];
+        char[] idCharPool = Tools.connectCharPool(Tools.generateCharPool('a', 'z'), Tools.generateCharPool('0', '9'));
         for (int i = 0; i < charId.length; i++)
             charId[i] = idCharPool[(int) (Math.random() * idCharPool.length)];
         //TODO check if already used
